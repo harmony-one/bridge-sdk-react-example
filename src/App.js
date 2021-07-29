f3ba256733269fe1769f43bdf2c0d4926fdf90d1
yuriy <yuriy.menkov@latoken.com>
2021-07-29 20:32:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import detectEthereumProvider from '@metamask/detect-provider'
+import { BridgeSDK, TOKEN, EXCHANGE_MODE, STATUS, NETWORK_TYPE } from 'bridge-sdk'
+import * as configs from 'bridge-sdk/lib/configs';
 
-function App() {
+const App = () => {
+  const [metamaskAddress, setMetamaskAddress] = useState()
+
+  useEffect(() => {
+    detectEthereumProvider().then((provider) => {
+      try {
+        // @ts-ignore
+        if (provider !== window.ethereum) {
+          console.error('Do you have multiple wallets installed?')
+        }
+
+        if (!provider) {
+          alert('Metamask not found')
+        }
+
+        provider.on('accountsChanged', (accounts) =>
+            setMetamaskAddress(accounts[0])
+        )
+
+        provider.on('disconnect', () => {
+            setMetamaskAddress('')
+        })
+
+        provider
+            .request({ method: 'eth_requestAccounts' })
+            .then(async (accounts) => {
+                setMetamaskAddress(accounts[0])
+            })
+      } catch (e) {
+        console.error(e)
+      }
+    })
+  }, [])
+
+  const sendTokens = async () => {
+      const bridgeSDK = new BridgeSDK({ logLevel: 2 }); // 2 - full logs, 1 - only success & errors, 0 - logs off
+
+      await bridgeSDK.init(configs.testnet);
+
+      await bridgeSDK.setUseMetamask(true);
+
+      let operationId;
+
+      // display operation status
+      let intervalId = setInterval(async () => {
+          if (operationId) {
+              const operation = await bridgeSDK.api.getOperation(operationId);
+
+              console.log(operation.status);
+              console.log(
+                'Action: ',
+                operation.actions.filter(a => a.status === STATUS.IN_PROGRESS)
+              );
+
+              if (operation.status !== STATUS.IN_PROGRESS) {
+                  clearInterval(intervalId);
+              }
+          }
+      }, 4000);
+
+      try {
+          await bridgeSDK.sendToken(
+              {
+                  type: EXCHANGE_MODE.ETH_TO_ONE,
+                  network: NETWORK_TYPE.ETHEREUM,
+                  token: TOKEN.BUSD,
+                  amount: 2,
+                  ethAddress: metamaskAddress,
+                  oneAddress: 'one1we0fmuz9wdncqljwkpgj79k49cp4jrt5hpy49j',
+              },
+              id => (operationId = id)
+          );
+      } catch (e) {
+          console.log('Error: ', e.message);
+      }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+          style={{
+            background: '#dedede',
+            width: '100vw',
+            height: '100vh',
+            padding: '100px 0'
+          }}
+      >
+        <div
+            style={{
+              maxWidth: 600,
+              margin: '0 auto'
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            Your metamask address: {metamaskAddress}
+            <button onClick={() => sendTokens()}>Send tokens</button>
+        </div>
+      </div>
+  )
 }
 
-export default App;
+export default App
